3990714d7a2e1e69ff0aed29cc12dbf950050b45
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-02-23 17:38:43 +0000

    Working on the search result list

--- client/main.js
+++ client/main.js
@@ -58,11 +58,38 @@ Template.home.events({
     'click .auto-target': function(event, template){
         var _this = event.currentTarget,
             dataSource = _this.getAttribute('data-source'),
-            playlist = template.find('#playlist');
+            playlist = template.find('#playlist'), feed = [],
+            resultBox = template.find('#resultBox'),
+            searchedTitle = _this.text,
+            searchField = template.find('#autocomp-field');
 
         Meteor.call('getYtList', dataSource, function(err, res){
             if(!err){
-                console.log(res);
+                searchField.value = searchedTitle;
+                res = $.parseJSON(res);
+                var items = res.items;
+                for (var i = 0; i < items.length; i++){
+
+                    //TODO implement response display
+                    console.log(items[i]);
+
+                    var snippet = items[i].snippet,
+                        current =
+                            '<li class="video-wrapper" data-videoId="'+ items[i].id.videoId +'"><img src="'+ snippet.thumbnails.default.url +'">' +
+                                '<a href="https://www.youtube.com/watch?v='+ items[i].id.videoId +'" class="title-link" target="_blank">'+ snippet.title +'</a>'+
+                                '<p class="description">'+ snippet.description +'</p>'+
+                                '<ul class="video-actions">' +
+                                    '<li class="favourite"><i class="fa fa-plus"></i></li>' +
+                                '</ul>'+
+                            '</li>';
+
+                    feed.push(current);
+                }
+                var resultList = document.createElement('ul');
+                resultList.innerHTML = feed.join(' ');
+                playlist.innerHTML = '';
+                playlist.appendChild(resultList);
+                resultBox.innerHTML = '';
             } else {
                 console.log(err);
             }
