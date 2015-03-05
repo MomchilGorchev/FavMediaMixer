2451769ee11112cd59a03f0f75d8490bf77021fa
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-03-05 18:00:45 +0000

    Working on the github feed

--- client/github.events.js
+++ client/github.events.js
@@ -4,7 +4,8 @@
 Template.github.events({
     'click #get-github-feed': function(e, t){
         var trigger = e.currentTarget,
-            container = t.find('.container'),
+            container = t.find('#github-result'),
+            containerBody = $(container).find('tbody');
             user = t.find('.github-user').value, buffer = [];
         console.log(user);
 
@@ -14,11 +15,22 @@ Template.github.events({
 
                 res = $.parseJSON(res);
 
+                console.log(res);
+
                 $.each(res, function(){
                     var curr = this;
-                    buffer.push('<p>'+curr.actor.login+' - '+ curr.type +' - '+ curr.repo.name +'</p>');
+                    buffer.push(
+                        '<tr>' +
+                            '<td><a href="https://github.com/'+ curr.actor.login +'">' +
+                                '<img src="https://avatars.githubusercontent.com/u/'+
+                                curr.actor.id +'" class="user-avatar" />'+ curr.actor.login +'</a></td>' +
+                            '<td>'+ curr.type +'</td>' +
+                            '<td>'+ curr.repo.name +'</td>' +
+                        '</tr>'
+                    );
                 });
-                $(container).html(buffer);
+                $(containerBody).html(buffer);
+                $(container).removeClass('hidden');
             });
         }
 
