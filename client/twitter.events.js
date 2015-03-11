1d718cf19cad7f59c53d0e0e9c73c8e97581e4ad
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-03-11 23:51:09 +0000

    Submit on Enter key

--- client/twitter.events.js
+++ client/twitter.events.js
@@ -1,45 +1,52 @@
 Template.twitter.events({
-     'click #get-tw-feed': function(e, t){
-         var trigger = $(e.currentTarget),
-             wrapper = t.find('#tweets-wrapper'),
-             query = t.find('.tw-search').value, tweets = [];
-         // Validate
-         if(query && query.length){
-             $(trigger).attr('disabled', true).html('<i class="fa fa-spinner fa-spin"></i>');
-             // Call server
-             Meteor.call('clearTempCollection', 'TweetsTemp', function(err, res){
-                 if(err){
-                     console.log(err);
-                 }
-             });
-             Meteor.call('twSearchApi', query, function(err, res){
-                if (err){
-                    // If error display error message
-                    FlashMessages.sendError('Error: '+ err.error);
-                    $(trigger).removeAttr('disabled').html('<i class="fa fa-cloud-download"></i> Get Feed');
-                } else {
-                    // Else handle the result
-                    $.each(res.statuses, function(k, v){
-                        // Display the API object for each tweet
-                        // console.log(this);
-                        var _this = this,
-                            twData = {
-                                userId: Meteor.user()._id,
-                                text: _this.text,
-                                user: _this.user,
-                                created: _this.created_at
-                            };
-                        tweets.push(twData);
-                    });
-                    Meteor.call('addTweets', tweets, function(err, res){
-                       if(err){
-                           FlashMessages.sendError('<i class="fa fa-warning"></i> Error occurred while proccessing your request. <br />Please try again');
-                       } else {
-                           $(trigger).removeAttr('disabled').html('<i class="fa fa-cloud-download"></i> Get Feed');
-                       }
-                    });
-                }
-             });
+     'click #get-tw-feed, keyup .tw-search': function(e, t){
+         var query = null,  trigger = $(e.currentTarget),
+             wrapper = t.find('#tweets-wrapper'), tweets = [];
+         if(e.keyCode === 13 || e.type === 'click'){
+             if(trigger.hasClass('tw-search')){
+                 trigger = t.find('#get-tw-feed');
+             }
+             query = t.find('.tw-search').value;
+             // Validate
+             if(query && query.length){
+                 $(trigger).attr('disabled', true).html('<i class="fa fa-spinner fa-spin"></i>');
+                 // Call server
+                 Meteor.call('clearTempCollection', 'TweetsTemp', function(err, res){
+                     if(err){
+                         console.log(err);
+                     }
+                 });
+                 Meteor.call('twSearchApi', query, function(err, res){
+                     if (err){
+                         // If error display error message
+                         FlashMessages.sendError('Error: '+ err.error);
+                         $(trigger).removeAttr('disabled').html('<i class="fa fa-cloud-download"></i> Get Feed');
+                     } else {
+                         // Else handle the result
+                         $.each(res.statuses, function(k, v){
+                             // Display the API object for each tweet
+                             // console.log(this);
+                             var _this = this,
+                                 twData = {
+                                     userId: Meteor.user()._id,
+                                     text: _this.text,
+                                     user: _this.user,
+                                     created: _this.created_at
+                                 };
+                             tweets.push(twData);
+                         });
+                         Meteor.call('addTweets', tweets, function(err, res){
+                             if(err){
+                                 FlashMessages.sendError('<i class="fa fa-warning"></i> Error occurred while proccessing your request. <br />Please try again');
+                             } else {
+                                 $(trigger).removeAttr('disabled').html('<i class="fa fa-cloud-download"></i> Get Feed');
+                             }
+                         });
+                     }
+                 });
+             }
          }
+
+
      }
 });
--- server/kadira.js
+++ server/kadira.js
@@ -1,2 +1,2 @@
 // Kadira Access key
-Kadira.connect('wfxxXvwjYyRa8kYay', 'ea53f7ec-6741-4ee8-8625-5e71c2368535');
+//Kadira.connect('wfxxXvwjYyRa8kYay', 'ea53f7ec-6741-4ee8-8625-5e71c2368535');
